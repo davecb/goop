7e08ebe1f5392579fb6ee9b73a70f2b3130721db
David Collier-Brown <davecb@spamcop.net>
2025-07-06 09:48:33 -0400

    Clean up structure, stop work on racer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# goop, Go Object Print
+# goop, Go Object Printer
 
 This is a blatant copy of Rust oDB, a printer you use for debugging, sorta.
 
@@ -7,22 +7,26 @@ using them based on a debug flag, and then throwing them away?
 
 This is a program that calls an http endpoint that returns a stuct in json, so the program can pretty-print it.
 
-The interface with the program is trivial and localized, so you don't need to delet it and then reinvent it every time.
+The interface with the program is trivial and localized, so you don't need to delete it and then reinvent it every time.
 
 
 ## Synopsis
-goop [-opts] url command
+goop [-opts] url [command]
 
 ## Description
-Sends a command to a program via http, to print a named thing. Pretty-prints the returned json.
+Sends a command to a program via http, to print a named thing. Pretty-prints the returned json. 
+
+If a command is not provided on the command-line, the program loops, prompting the user each time.
 
 ## Files
+
+## Example
 
 ## See Also
 
 ## Bugs
 
-The real oDB can do iterations: I'm not doing that for the MVP. They're easy to add.
+The real oDB can do iterations: I'm not doing that for the MVP. They may be easy to add.
 
 ## Diagnostics
 
--- pkg/pipe/piper.go
+++ pkg/pipe/piper.go
@@ -1,11 +1,11 @@
 /* 
  * piper: a program to use channels to mitigate a data-race.
- * 	After updating a struct, and IFF a debugging variable
- *	set, write the struct to a pipe.
+ * 	After updating a struct, and IFF an atomic boolean is
+ *	set, writes the struct to a pipe.
  *	That pipe goes to a goroutine that implements read-
  *	copy update.
  *	A select statement there mediates the race, by allowing
- *	either a read or a write to the copy of the data, but
+ *	either a read or write to the copy of the data, but
  *	not both.
  */
 
@@ -20,7 +20,7 @@
  *	to the "input" channel.  When that happens, the read 
  *	suceeds and the handler turns off the variable.
  *	The goroutine needs to get the output request, and
- *	give it a non-nil struct that arrived on the input
+ *	give it a non-nil struct that arrived at the input
  *	channel... somehow. Probably by being handed a request
  *	on a "start now" channel that sets the variable to nil,
  *	and the output channel won't send a <nothing>.
@@ -32,4 +32,9 @@
 
  // goroutine with three channels
 
+/*
+ * Over in cmd, a command-line program looks at the variable to read,
+ * selects the right path, does an http request and pretty-prints the
+ * json it gets back.  Then loops.
+ */
 
